8d7276567673f66a35eb3507804fcd5b66750c12
Danely Trujillo <danelytrujillo99@gmail.com>
2021-10-20 21:16:11 -0500

    Pagina Jugadores parcial segundo corte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { APP_ROUTING } from './app.routes';
+
+import { jugadoresService } from './servicios/jugadores.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JugadoresComponent } from './components/jugadores/jugadores.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { Jugador1Component } from './components/jugador1/jugador1.component';
+import { Jugador2Component } from './components/jugador2/jugador2.component';
+import { Jugador3Component } from './components/jugador3/jugador3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JugadoresComponent,
+    FooterComponent,
+    HeaderComponent,
+    Jugador1Component,
+    Jugador2Component,
+    Jugador3Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [jugadoresService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
